57f6c7b8998b469caf50abe9a5dfce29b078bc98
Doun <daniel.esco92@gmail.com>
2025-08-17 17:28:43 +0200

    summary works

--- BlizzardAPI.py
+++ BlizzardAPI.py
@@ -1,32 +1,88 @@
 import requests
 from dotenv import load_dotenv
-import os
+from os import getenv
 
 class BlizzardApi:
     
     def __init__(self):
-        pass
+        self.access_token = self._request_token()
+        # self.oauth_token = self.exchange_code_for_token()
+        self.basic_url = "https://eu.api.blizzard.com/"
+        self.token_url = "https://oauth.battle.net/token"
     
     def __str__(self):
-        return "Oui"
+        return "Connecté"
     
-    def request_token(self):
+    def _request_token(self):
+        """
+        Permet de générer une réponse JSON avec la clé d'accès à l'API publique.
+
+        Returns:
+            JSON: La clé d'accès à l'API publique
+        """
         # Load secret .env file
         load_dotenv()
         
         # Define Crendetials
-        AUTH_1 = os.getenv('AUTH_1')
-        AUTH_2 = os.getenv('AUTH_2')
+        client_id = getenv('Client_ID')
+        client_secret = getenv('Client_Secret')
         
         data = {
             'grant_type': 'client_credentials',
         }
 
         access_token_request = requests.post('https://oauth.battle.net/token',
-                                data=data, auth=(AUTH_1, AUTH_2))
+                                data=data, auth=(client_id, client_secret))
         
         access_token = access_token_request.json()
+        return access_token
+    
+    def exchange_code_for_token(self, code:str, redirect_uri: str):
+        """
+        Échange un code temporaire contre un access_token utilisable.
+        """
+        # Load secret .env file
+        load_dotenv()
+        
+        # Define Crendetials
+        client_id = getenv('Client_ID')
+        client_secret = getenv('Client_Secret')
+        
+        
+        data = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": redirect_uri
+        }
+        
+        response = requests.post(
+            self.token_url,
+            data=data,
+            auth=(client_id, client_secret)
+        )
+    
+        if response.status_code != 200:
+            raise Exception(f"Erreur {response.status_code}: {response.text}")
 
-        print(access_token['access_token'])
+        token_data = response.json()
+        print(token_data)
+        self.access_token = token_data["access_token"]
+        
+        return token_data
+    
+    def get_profile_summary(self):
+        headers = {
+            "Authorization": f"Bearer {self.access_token}"
+        }
+        params = {
+            "namespace": "profile-eu",
+            "locale": "fr_FR"
+        }
+        
+        url = f"{self.basic_url}/profile/user/wow"
+        response = requests.get(url, headers=headers, params=params)
+        
+        if response.status_code != 200:
+            raise Exception(f"Erreur {response.status_code}: {response.text}")
 
-        return access_token['access_token']
+        return response.json()
--- get_char_data.py
+++ get_char_data.py
@@ -1,6 +1,16 @@
 from BlizzardAPI import BlizzardApi
 
+# Connect to the object
 blizzard_api = BlizzardApi()
-print(blizzard_api)
+print(blizzard_api.access_token)
 
-request = blizzard_api.request_token()
+
+code = "EUIKZRD7QQJGN34I4EKUM5GK68WXTIR867"
+redirect_uri = "http://localhost:5000/callback"
+
+token_data = blizzard_api.exchange_code_for_token(code, redirect_uri)
+print("Token reçu :", token_data)
+
+profile = blizzard_api.get_profile_summary()
+# Si blizzard_api.get-profile_summary() ne fonctionne pas, je dois refaire
+# https://oauth.battle.net/authorize?client_id=<CLIENT_ID&redirect_uri=http://localhost:5000/callback&response_type=code&scope=wow.profile
